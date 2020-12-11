92f743df1326bed2b2e0d6d0aa53d39ea08efef5
shldhll <iam.shldhll@gmail.com>
2020-12-12 05:16:25 +0530

    os interface functions

--- system/system_windows.go
+++ system/system_windows.go
@@ -1,1 +1,19 @@
 package system
+
+import "time"
+
+// Current represents the current operating system
+type Current struct{}
+
+// GetApplicationName returns the name of current forground application
+func (c Current) GetApplicationName() (appName string) {
+	return appName
+}
+
+// Now returns current time
+func (c Current) Now() time.Time {
+	return time.Now()
+}
+
+// Log is used for system specific logging
+func (c Current) Log(msg string) {}
