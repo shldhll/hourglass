bb628f8906e339de53c530c9190a1b9266d8a003
shldhll <iam.shldhll@gmail.com>
2020-12-14 01:49:06 +0530

    active application name

--- system/system_linux.go
+++ system/system_linux.go
@@ -1,12 +1,45 @@
 package system
 
-import "time"
+import (
+	"os/exec"
+	"strings"
+	"time"
+)
+
+const (
+	windowIDSplitSep   = "_NET_ACTIVE_WINDOW(WINDOW): window id # "
+	windowNameSplitSep = "WM_NAME(UTF8_STRING) = "
+)
 
 // Current represents the current operating system
 type Current struct{}
 
 // GetApplicationName returns the name of current forground application
 func (c Current) GetApplicationName() (appName string) {
+	windowIDCmd, err := exec.Command("xprop", "-root", "_NET_ACTIVE_WINDOW").Output()
+	if err != nil {
+		return
+	}
+
+	windowIDCmdSplitRes := strings.Split(string(windowIDCmd), windowIDSplitSep)
+	if len(windowIDCmdSplitRes) <= 1 {
+		return
+	}
+
+	windowID := windowIDCmdSplitRes[1]
+	windowNameCmd, err := exec.Command("xprop", "-id", windowID, "WM_NAME").Output()
+	if err != nil {
+		return
+	}
+
+	windowNameCmdSplitRes := strings.Split(string(windowNameCmd), windowNameSplitSep)
+	if len(windowNameCmdSplitRes) <= 1 {
+		return
+	}
+
+	windowName := windowNameCmdSplitRes[1]
+	appName = windowName
+
 	return appName
 }
 
