c68134e745ddde74deacc78dbb6362b9c2259057
shldhll <iam.shldhll@gmail.com>
2020-12-20 23:04:19 +0530

    new task function

--- tracker/tracker.go
+++ tracker/tracker.go
@@ -1,6 +1,10 @@
 package tracker
 
-import "time"
+import (
+	"github.com/shldhll/hourglass/system"
+
+	"time"
+)
 
 // Task struct represents a running application.
 type Task struct {
@@ -16,4 +20,17 @@ func (t Task) Time() time.Time {
 // AppName returns the title of current application
 func (t Task) AppName() string {
 	return t.applicationName
+}
+
+// NewTask creates a new task
+func NewTask(appName string, recordedTime time.Time) *Task {
+	return &Task{
+		applicationName: appName,
+		recordedTime:    recordedTime,
+	}
+}
+
+// Ping returns window information in the form of Task struct.
+func Ping(o system.OS) *Task {
+	return NewTask(o.GetApplicationName(), o.Now())
 }
