00e746d886c6ea19f1fe68442a4edd6ce57fac13
shldhll <iam.shldhll@gmail.com>
2020-12-20 22:35:29 +0530

    appname and time functions

--- tracker/tracker.go
+++ tracker/tracker.go
@@ -7,3 +7,13 @@ type Task struct {
 	applicationName string
 	recordedTime    time.Time
 }
+
+// Time returns the capture Time
+func (t Task) Time() time.Time {
+	return t.recordedTime
+}
+
+// AppName returns the title of current application
+func (t Task) AppName() string {
+	return t.applicationName
+}
