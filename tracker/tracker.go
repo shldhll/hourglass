6ad6bdfb779b745fb6590c71a15bebc5c4f6e42a
shldhll <iam.shldhll@gmail.com>
2020-12-21 02:05:41 +0530

    creates id using task info

--- tracker/tracker.go
+++ tracker/tracker.go
@@ -4,6 +4,19 @@ import (
 	"github.com/shldhll/hourglass/system"
 
 	"time"
+	"strings"
+	"fmt"
+)
+
+const (
+	// EntryIDStringFormat is the format used for Sprintf function to create the ID for database entry
+	EntryIDStringFormat = "%v_%s"
+	// EntryIDDateFormat is the date format used in the ID
+	EntryIDDateFormat = "2006-01-02"
+	// EntryIDNameReplaceOld is the string which is to be replaced by EntryIDNameReplaceNew
+	EntryIDNameReplaceOld = " "
+	// EntryIDNameReplaceNew is the string which replaces EntryIDNameReplaceOld
+	EntryIDNameReplaceNew = ""
 )
 
 // Task struct represents a running application.
@@ -33,4 +46,12 @@ func NewTask(appName string, recordedTime time.Time) *Task {
 // Ping returns window information in the form of Task struct.
 func Ping(o system.OS) *Task {
 	return NewTask(o.GetApplicationName(), o.Now())
+}
+
+// CreateID creates an ID string using formatted date and string
+func CreateID(appName string, date time.Time) string {
+	formattedDate := date.Format(EntryIDDateFormat)
+	formattedAppName := strings.ReplaceAll(appName, EntryIDNameReplaceOld, EntryIDNameReplaceNew)
+
+	return fmt.Sprintf(EntryIDStringFormat, formattedDate, formattedAppName)
 }
