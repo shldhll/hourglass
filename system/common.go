be7445855b7d82aaf1528eb14787dea3f60136a4
shldhll <iam.shldhll@gmail.com>
2020-12-21 20:20:32 +0530

    system config implementation

--- system/common.go
+++ system/common.go
@@ -16,3 +16,30 @@ type Config interface {
 	LoopCheck() bool
 	LoopNext()
 }
+
+// Cfg implements Config interface
+type Cfg struct {
+	cooldownTime  time.Duration
+	minUsageTime  time.Duration
+	loopCheckBool bool
+}
+
+// GetCooldownTime returns cooldown duration
+func (c Cfg) GetCooldownTime() time.Duration {
+	return c.cooldownTime
+}
+
+// GetMinUsageTime returns minimum time duration
+func (c Cfg) GetMinUsageTime() time.Duration {
+	return c.minUsageTime
+}
+
+// LoopCheck replicates a custom loop condition check
+func (c Cfg) LoopCheck() bool {
+	return c.loopCheckBool
+}
+
+// LoopNext is called before each loop
+func (c Cfg) LoopNext() {
+	time.Sleep(c.GetCooldownTime())
+}
