090ddcd60b94994347c9f1a8e418ddaab18b864f
shldhll <iam.shldhll@gmail.com>
2020-12-22 22:35:31 +0530

    stub os

--- tracker/tracker_test.go
+++ tracker/tracker_test.go
@@ -1,5 +1,9 @@
 package tracker_test
 
+import "time"
+
+var stubTime = time.Date(1970, 01, 01, 0, 0, 0, 0, time.UTC)
+
 type stubOS struct {
 	applicationName  string
 	realTime         bool
@@ -7,4 +11,23 @@ type stubOS struct {
 	nowCalled        int
 	shouldLog        int
 	logChan          chan string
+}
+
+func (s *stubOS) GetApplicationName() string {
+	s.getAppNameCalled++
+	return s.applicationName
+}
+
+func (s *stubOS) Now() time.Time {
+	s.nowCalled++
+	if s.realTime {
+		return time.Now()
+	}
+	return stubTime
+}
+
+func (s *stubOS) Log(msg string) {
+	if s.shouldLog != 0 {
+		s.logChan <- msg
+	}
 }
