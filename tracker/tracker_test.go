b58ec2618b5bcf263a3a662d9922e31aa21eb2bf
shldhll <iam.shldhll@gmail.com>
2020-12-22 22:45:05 +0530

    stub db

--- tracker/tracker_test.go
+++ tracker/tracker_test.go
@@ -1,8 +1,16 @@
 package tracker_test
 
-import "time"
+import (
+	"github.com/shldhll/hourglass/data"
 
-var stubTime = time.Date(1970, 01, 01, 0, 0, 0, 0, time.UTC)
+	"time"
+	"errors"
+)
+
+var (
+	stubTime = time.Date(1970, 01, 01, 0, 0, 0, 0, time.UTC)
+	stubDBWriteErr = errors.New("Error occurred while writing data")
+)
 
 type stubOS struct {
 	applicationName  string
@@ -30,4 +38,30 @@ func (s *stubOS) Log(msg string) {
 	if s.shouldLog != 0 {
 		s.logChan <- msg
 	}
+}
+
+type stubDB struct {
+	showErrorOK int
+	write       int
+	read        int
+	writeList   int
+	readList    int
+}
+
+func (s *stubDB) Write(entry data.Entry) error {
+	s.write++
+	if s.showErrorOK == 0 {
+		return nil
+	}
+
+	return stubDBWriteErr
+}
+
+func (s *stubDB) Read(key string) (data.Entry, error) {
+	s.read++
+	if s.showErrorOK != 0 {
+		return data.Entry{}, stubDBWriteErr
+	}
+
+	return data.Entry{}, nil
 }
